d0c9a3c27931895eddafd3486d7fde3475902f07
bek02k <Juankings321@gmail.com>
2026-05-19 11:17:59 -0300

    commit antes do desafio

---
+++ js/api.js
@@ -0,0 +1,14 @@
+// api.js
+export async function buscarDica() {
+  try {
+    const resposta = await fetch("https://api.adviceslip.com/advice");
+    if (!resposta.ok) {
+      throw new Error("Erro ao buscar dica externa");
+    }
+
+    const dados = await resposta.json();
+    return dados.slip.advice;
+  } catch (erro) {
+    return null;
+  }
+}
